54d07a1367f9dd6d33e05cd8f191f03bfc3021ac
Skallywagg <lee.sam1492@gmail.com>
2020-03-16 02:55:48 -0600

    rental complete for now

--- src/Rental/Rental.jsx
+++ src/Rental/Rental.jsx
@@ -12,39 +12,115 @@ class ServerRental extends PureComponent {
                </div>
 
                 <div className="features-container">
-                    <div className='features-con'>
+
+                    <div className='features-card'>
                         <div className='icon-features'>
                             <div className='feat-icon-con'>
                                     <i className="far fa-hdd feat-icon"></i>
                             </div> 
                         </div>
-                        <div>
-                            High Speed ssd's
+                        <div className='icon-text'>
+                            High speed ssd's
                         </div>
                     </div>
 
-                    <div className='features-con'>
+                    <div className='features-card'>
                         <div className='icon-features'>
                             <div className='feat-icon-con'>
-                                    <i className="far fa-hdd feat-icon"></i>
+                            <i className="fas fa-cogs feat-icon"></i>
                             </div> 
                         </div>
-                        <div>
-                            
+                        <div className='icon-text'>
+                            No Setup
                         </div>
                     </div>
 
-                    <div className='features-con'>
+                    <div className='features-card'>
                         <div className='icon-features'>
                             <div className='feat-icon-con'>
-                                    <i className="far fa-hdd feat-icon"></i>
+                            <i class="fas fa-shield-alt feat-icon"></i>
                             </div> 
                         </div>
-                        <div>
-                            High Speed ssd's
+                        <div className='icon-text'>
+                           Ddos Protection
                         </div>
+                      </div> 
+                </div>
+
+                <div className="features-container">
+
+                    <div className='features-card'>
+                        <div className='icon-features'>
+                            <div className='feat-icon-con'>
+                            <i class="fas fa-globe feat-icon"></i>
+                            </div> 
+                        </div>
+                        <div className='icon-text'>
+                            Global Locations
+                        </div>
+                    </div>
+
+                    <div className='features-card'>
+                        <div className='icon-features'>
+                            <div className='feat-icon-con'>
+                            <i class="fas fa-headset feat-icon"></i>
+                            </div> 
+                        </div>
+                        <div className='icon-text'>
+                            24/7 Support
+                        </div>
+                    </div>
+
+                    <div className='features-card'>
+                        <div className='icon-features'>
+                            <div className='feat-icon-con'>
+                            <i class="fas fa-tachometer-alt feat-icon"></i>
+                            </div> 
+                        </div>
+                        <div className='icon-text'>
+                            High Speed
+                        </div>
+                      </div> 
+                </div>
+                <div className='pricing-card'>
+                    <div className='price'>
+                        $6.99 - 2/hrs
+                    </div>
+                    <div className='rental-btn'>
+                        Order now
                     </div>
                 </div>
+
+                <div className='pricing-card'>
+                    <div className='price'>
+                        $12.99 - 4/hrs
+                    </div>
+                    <div className='rental-btn'>
+                        Order now
+                    </div>
+                </div>
+               
+                <div className='pricing-card'>
+                    <div className='price'>
+                        $16.99 - 6/hrs
+                    </div>
+                    <div className='rental-btn'>
+                        Order now
+                    </div>
+                </div>
+
+                <div className='pricing-card'>
+                    <div className='price'>
+                        $24.99 - 8/hrs
+                    </div>
+                    <div className='rental-btn'>
+                        Order now
+                    </div>
+                </div>
+
+                
+
+                
 
 
         
