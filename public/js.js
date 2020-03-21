e171abecd687fba01d13287965a2c1d040f85ccd
Skallywagg <lee.sam1492@gmail.com>
2020-03-20 23:50:09 -0600

    code

---
+++ public/js.js
@@ -0,0 +1,18 @@
+// js fundamentals //
+
+// variable mutation and type coercion //
+/* */
+var firstName = 'sam'
+var age = 27;
+
+console.log(firstName + ' ' + age);
+// declare multiple values with a var//
+var job, isMarried; 
+job = 'teacher';
+isMarried = false;
+
+console.log(firstName + ' is a ' + age + ' year old' + job + '. is he married? ' + isMarried)
+
+
+
+
